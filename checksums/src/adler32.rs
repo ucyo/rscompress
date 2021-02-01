b348726b0870e898b686c915c51c60e0fa01ee60
ucyo <cayoglu@me.com>
2021-02-01 22:46:08 +0100

    Make tests pass

--- checksums/src/adler32.rs
+++ checksums/src/adler32.rs
@@ -1,4 +1,32 @@
 
+use super::Checksum;
+struct Adler32 {
+    a: u16,
+    b: u16,
+}
+
+impl Adler32 {
+    fn new() -> Self {
+        Adler32 {
+            a: 1,
+            b: 0,
+        }
+    }
+}
+
+impl Checksum for Adler32 {
+    fn update(&mut self, data: &[u8]) {
+        for byte in data.iter() {
+            self.a += *byte as u16 % 65535;
+            self.b += self.a % 65535;
+            println!("{:?} {:?}", self.a, self.b)
+        }
+    }
+    fn checksum(&self) -> u32 {
+        ((self.b as u32) << 16) | self.a as u32
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
