e7a1920c9231c37c0b2f18d3178f50faf6096003
ucyo <cayoglu@me.com>
2021-02-04 22:27:02 +0100

    Add some documentation

--- checksums/src/lib.rs
+++ checksums/src/lib.rs
@@ -1,15 +1,26 @@
+//! # rscompress-checksums
+//!
+//! `rscompress_checksums` implements dfferent checksum algorithms for bytes.
+//! checksums can be added to the compressed data.
+//! This can then be used to check up if the deconstructed data is the same
+//! as the original data.
+
 mod adler32;
 use std::error::Error;
 use std::fmt;
 
+/// Trait for calculating checksums from binary data
 trait Checksum {
     fn update(&mut self, data: &[u8]) -> Option<usize>;
     fn checksum(&self) -> Result<u32, ChecksumError>;
 }
 
+/// An enum representing possible errors during checksum calculation
 #[derive(Debug)]
 enum ChecksumError {
+    /// The final checksum can not be calculated
     NoChecksum,
+    /// Error during updating of the checksums
     UpdateError,
 }
 
