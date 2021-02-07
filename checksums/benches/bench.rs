298f57d721aec57f02e04684884cd6eca7dd68b6
ucyo <cayoglu@me.com>
2021-02-07 21:31:27 +0100

    Extend tests with several checksums

--- checksums/benches/bench.rs
+++ checksums/benches/bench.rs
@@ -2,18 +2,23 @@ use criterion::{criterion_group, criterion_main};
 use criterion::{Criterion, BenchmarkId};
 use rscompress_checksums::{Adler32, Checksum};
 
-fn do_adler32_checksum(data: &str) -> u32{
+fn do_adler32_checksum(data: &[u8]) -> u32{
     let mut a = Adler32::new();
-    a.update(data.as_bytes());
+    a.update(data);
     return a.checksum().unwrap()
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
-    let word = "Wikipedia";
-    let id = BenchmarkId::new("word", word);
-    c.bench_with_input(id, &word, |b, &s| {
-        b.iter(|| do_adler32_checksum(s));
-    });
+    let words = [
+        "Wikipedia", "Awesome-string-baby", "This is great"
+    ];
+    let mut group = c.benchmark_group("words");
+    for word in words.iter() {
+        let id = BenchmarkId::from_parameter(word);
+        group.bench_with_input(id, &word.as_bytes(), |b, &s| {
+            b.iter(|| do_adler32_checksum(s));
+        });
+    }
 }
 
 criterion_group!(benches, criterion_benchmark);
