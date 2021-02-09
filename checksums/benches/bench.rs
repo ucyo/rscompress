291a3889143ddd04c8cba492675f9a83bc76aa62
ucyo <cayoglu@me.com>
2021-02-09 22:57:15 +0100

    Add benchmarks

--- checksums/benches/bench.rs
+++ checksums/benches/bench.rs
@@ -1,20 +1,25 @@
 use criterion::{criterion_group, criterion_main};
 use criterion::{BenchmarkId, Criterion};
-use rscompress_checksums::{Adler32, Checksum};
+use rscompress_checksums::{Adler32, Checksum, CRC32};
 
-fn do_adler32_checksum(data: &[u8]) -> u32 {
-    let mut a = Adler32::new();
-    a.update(data);
-    return a.checksum().unwrap();
+fn do_checksum_checks(data: &[u8], model: &mut impl Checksum) -> u32 {
+    model.update(data);
+    return model.checksum().unwrap();
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
     let words = ["Wikipedia", "Awesome-string-baby", "This is great"];
     let mut group = c.benchmark_group("words");
     for word in words.iter() {
-        let id = BenchmarkId::from_parameter(word);
+        let id = BenchmarkId::new("Adler32", word);
+        let mut model = Adler32::new();
         group.bench_with_input(id, &word.as_bytes(), |b, &s| {
-            b.iter(|| do_adler32_checksum(s));
+            b.iter(|| do_checksum_checks(s, &mut model));
+        });
+        let id = BenchmarkId::new("CRC32", word);
+        let mut model = CRC32::new();
+        group.bench_with_input(id, &word.as_bytes(), |b, &s| {
+            b.iter(|| do_checksum_checks(s, &mut model));
         });
     }
 }
