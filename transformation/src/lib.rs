03286ffe56b4fd5726fde90056c78be8b8cedfe0
ucyo <cayoglu@me.com>
2021-02-11 00:04:11 +0100

    Add roundtrip macro

--- transformation/src/lib.rs
+++ transformation/src/lib.rs
@@ -31,3 +31,17 @@ macro_rules! test_reverse {
         }
     };
 }
+
+#[macro_export]
+macro_rules! test_roundtrip {
+    ($func_name:ident, $method:ident, $input:expr) => {
+        #[test]
+        fn $func_name() {
+            let mut rl = $method::new();
+            let tmp = rl.transform(&$input).unwrap();
+            let result = rl.reverse(&tmp).unwrap();
+
+            assert_eq!(result, $input)
+        }
+    };
+}
