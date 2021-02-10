32b5e629e4ae17da31cedfbc805a6b07d6a4f3b3
ucyo <cayoglu@me.com>
2021-02-10 23:12:38 +0100

    Implement run length transform (missing reverse)

--- transformation/src/lib.rs
+++ transformation/src/lib.rs
@@ -1,7 +1,19 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+mod runlength;
+
+pub trait Transform {
+    fn transform(&mut self, source: &[u8]) -> Option<Vec<u8>>;
+    fn rtransform(&mut self, source: &[u8]) -> Option<Vec<u8>>;
+}
+
+#[macro_export]
+macro_rules! test_transform {
+    ($func_name:ident, $method:ident, $input:expr, $expected:expr) => {
+        #[test]
+        fn $func_name() {
+            let mut rl = $method::new();
+            let result = rl.transform(&$input).unwrap();
+
+            assert_eq!(result, $expected)
+        }
+    };
 }
