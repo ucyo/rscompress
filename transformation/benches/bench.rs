2af20d08886d7bb1f0e503e57bd266b2a3eb361f
ucyo <cayoglu@me.com>
2021-02-11 22:20:36 +0100

    Perfect benchmarking setup

--- transformation/benches/bench.rs
+++ transformation/benches/bench.rs
@@ -1,17 +1,18 @@
 use criterion::{criterion_group, criterion_main};
 use criterion::{BenchmarkId, Criterion, Throughput};
-use rscompress_transformation::{RunLength, Transform};
 use rand::{rngs::OsRng, RngCore};
+use rscompress_transformation::{RunLength, Transform};
 
 const DATA_SIZE: usize = 10_000;
+const FACTORS: [usize; 5] = [2, 4, 8, 16, 32];
 
-fn do_transformation<M: Transform+Default>(data: &[u8]) -> Vec<u8> {
+fn do_transformation<M: Transform + Default>(data: &[u8]) -> Vec<u8> {
     let mut model: M = Default::default();
     let result = model.transform(data).unwrap();
-    return result
+    return result;
 }
 
-fn do_reverse<M: Transform+Default>(data: &[u8]) -> Vec<u8> {
+fn do_reverse<M: Transform + Default>(data: &[u8]) -> Vec<u8> {
     let mut model: M = Default::default();
     model.reverse(data).unwrap()
 }
@@ -24,23 +25,36 @@ fn generate_random_data(size: usize) -> Vec<u8> {
 
 fn criterion_transform(c: &mut Criterion) {
     let mut group = c.benchmark_group("transform");
-    let factors: [usize; 5] = [2, 4, 8, 16, 24];
-    for factor in factors.iter() {
+    for factor in FACTORS.iter() {
         let size = factor * DATA_SIZE;
         let data = generate_random_data(size);
         group.throughput(Throughput::Bytes((factor * DATA_SIZE) as u64));
-        group.bench_with_input(BenchmarkId::from_parameter(size), data.as_slice(), |b, s| {
-            b.iter(|| do_transformation::<RunLength>(s));
-        });
+        group.bench_with_input(
+            BenchmarkId::from_parameter(size),
+            data.as_slice(),
+            |b, s| {
+                b.iter(|| do_transformation::<RunLength>(s));
+            },
+        );
     }
+    group.finish();
 }
 
 fn criterion_reverse(c: &mut Criterion) {
-    let data = generate_random_data(DATA_SIZE);
-    let id = BenchmarkId::new("reverse", data.len());
-    c.bench_with_input(id, &data, |b, s| {
-        b.iter(|| do_reverse::<RunLength>(s))
-    });
+    let mut group = c.benchmark_group("reverse");
+    for factor in FACTORS.iter() {
+        let size = factor * DATA_SIZE;
+        let data = generate_random_data(size);
+        group.throughput(Throughput::Bytes((factor * DATA_SIZE) as u64));
+        group.bench_with_input(
+            BenchmarkId::from_parameter(size),
+            data.as_slice(),
+            |b, s| {
+                b.iter(|| do_reverse::<RunLength>(s));
+            },
+        );
+    }
+    group.finish();
 }
 
 criterion_group!(transform, criterion_transform);
