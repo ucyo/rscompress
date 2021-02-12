23e54825e384b4b322c9f52df02258a1788e8eb1
ucyo <cayoglu@me.com>
2021-02-12 11:31:18 +0100

    Add benchmarks

--- transformation/benches/bench.rs
+++ transformation/benches/bench.rs
@@ -1,7 +1,7 @@
 use criterion::{criterion_group, criterion_main};
 use criterion::{BenchmarkId, Criterion, Throughput};
 use rand::{rngs::OsRng, RngCore};
-use rscompress_transformation::{RunLength, Transform};
+use rscompress_transformation::{RunLength, MoveToFront, Transform};
 
 const DATA_SIZE: usize = 10_000;
 const FACTORS: [usize; 5] = [2, 4, 8, 16, 32];
@@ -30,10 +30,17 @@ fn criterion_transform(c: &mut Criterion) {
         let data = generate_random_data(size);
         group.throughput(Throughput::Bytes((factor * DATA_SIZE) as u64));
         group.bench_with_input(
-            BenchmarkId::from_parameter(size),
+            BenchmarkId::new("Run-Length", size),
             data.as_slice(),
             |b, s| {
                 b.iter(|| do_transformation::<RunLength>(s));
+            },
+        );
+        group.bench_with_input(
+            BenchmarkId::new("Move-to-Front", size),
+            data.as_slice(),
+            |b, s| {
+                b.iter(|| do_transformation::<MoveToFront>(s));
             },
         );
     }
@@ -47,16 +54,22 @@ fn criterion_reverse(c: &mut Criterion) {
         let data = generate_random_data(size);
         group.throughput(Throughput::Bytes((factor * DATA_SIZE) as u64));
         group.bench_with_input(
-            BenchmarkId::from_parameter(size),
+            BenchmarkId::new("Run-Length", size),
             data.as_slice(),
             |b, s| {
                 b.iter(|| do_reverse::<RunLength>(s));
+            },
+        );
+        group.bench_with_input(
+            BenchmarkId::new("Move-To-Front", size),
+            data.as_slice(),
+            |b, s| {
+                b.iter(|| do_reverse::<MoveToFront>(s));
             },
         );
     }
     group.finish();
 }
 
-criterion_group!(transform, criterion_transform);
-criterion_group!(reverse, criterion_reverse);
-criterion_main!(transform, reverse);
+criterion_group!(transform, criterion_transform, criterion_reverse);
+criterion_main!(transform);
