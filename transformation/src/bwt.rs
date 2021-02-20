5aa5ccbee6cd5344f64329b1e082fd4bf65ff320
ucyo <cayoglu@me.com>
2021-02-20 17:58:26 +0100

    Add bwt transform implementation

--- transformation/src/bwt.rs
+++ transformation/src/bwt.rs
@@ -1,7 +1,8 @@
 use std::todo;
-
+use std::str;
 use suffix;
 use crate::{Transform, TransformError};
+use log::debug;
 
 #[derive(Debug)]
 pub struct BurrowWheeler {
@@ -25,7 +26,23 @@ impl Default for BurrowWheeler {
 
 impl Transform for BurrowWheeler {
     fn transform(&mut self, source: &[u8]) -> Result<Vec<u8>, TransformError> {
-        todo!()
+        let mut result = Vec::with_capacity(source.len());
+        let temp = str::from_utf8(source).unwrap();
+        let s = suffix::SuffixTable::new(temp);
+        let table = s.table();
+        debug!("Suffixtable: {:?} ({})", table, table.len());
+        for ix in table.iter() {
+            if *ix as usize == 0 {
+                let val = source[source.len() - 1];
+                result.push(val);
+        } else {
+                let val = source[*ix as usize - 1];
+                result.push(val);
+        }
+        }
+        self.ix = table.iter().position(|&x| x==0);
+        debug!("Suffixtable Index Position: {:?}", self.ix);
+        Ok(result)
     }
     fn reverse(&mut self, source: &[u8]) -> Result<Vec<u8>, TransformError> {
         todo!()
@@ -40,6 +57,7 @@ mod tests {
 
     #[test]
     fn test_easy_transforms() {
+        transform::<BurrowWheeler>("banana".as_bytes(), "nnbaaa".as_bytes());
         transform::<BurrowWheeler>("compressioncode".as_bytes(), "neodrsooccimpse".as_bytes());
     }
 
