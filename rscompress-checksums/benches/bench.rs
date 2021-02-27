e3b57cfbcdd3ffe2b517018628b91a18e866f003
ucyo <cayoglu@me.com>
2021-02-27 15:07:50 +0100

    Reformat benchmarks

--- rscompress-checksums/benches/bench.rs
+++ rscompress-checksums/benches/bench.rs
@@ -3,13 +3,7 @@ use criterion::{BenchmarkId, Criterion, Throughput};
 use rscompress_checksums::{Adler32, Checksum, CRC32};
 
 const MIN_DATA_SIZE: usize = 1_000;
-const FACTORS: [usize; 5] = [1, 10, 100, 1_000, 10_000];
-
-fn do_checksum<M: Checksum + Default>(data: &[u8]) -> u32 {
-    let mut model: M = Default::default();
-    model.update(&data);
-    model.checksum().unwrap()
-}
+const FACTORS: [usize; 5] = [1, 5, 10, 50, 100];
 
 fn criterion_benchmark(c: &mut Criterion) {
     let mut group = c.benchmark_group("checksums");
@@ -17,16 +11,29 @@ fn criterion_benchmark(c: &mut Criterion) {
     for factor in FACTORS.iter() {
         let size = factor * MIN_DATA_SIZE;
         let data: Vec<u8> = source.iter().take(size).map(|x| *x).collect();
+        let mut tmp = 0u32;
         group.throughput(Throughput::Bytes(size as u64));
+
+        // Testing Adler32 checksums
+        let mut model = Adler32::new();
         group.bench_with_input(
             BenchmarkId::new("Adler32", size),
             data.as_slice(),
             |b, s| {
-                b.iter(|| do_checksum::<Adler32>(s));
+                b.iter(|| {
+                    model.update(&s);
+                    tmp = model.checksum().unwrap()
+                });
             },
         );
+
+        // Testing Adler32 checksums
+        let mut model = CRC32::new();
         group.bench_with_input(BenchmarkId::new("CRC32", size), data.as_slice(), |b, s| {
-            b.iter(|| do_checksum::<CRC32>(s));
+            b.iter(|| {
+                model.update(&s);
+                tmp = model.checksum().unwrap()
+            });
         });
     }
     group.finish();
