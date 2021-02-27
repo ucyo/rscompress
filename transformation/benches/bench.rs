748eaab88875677fdbc001b1b85836fa5eff14a5
ucyo <cayoglu@me.com>
2021-02-27 09:37:46 +0100

    Update benchmarking methods to accomodate info transformation

--- transformation/benches/bench.rs
+++ transformation/benches/bench.rs
@@ -3,75 +3,70 @@ use criterion::{BenchmarkId, Criterion, Throughput};
 use rscompress_transformation::{BurrowWheeler, MoveToFront, RunLength, Transform};
 
 const MIN_DATA_SIZE: usize = 1_000;
-const FACTORS: [usize; 5] = [1, 10, 100, 1_000, 10_000];
+const FACTORS: [usize; 5] = [1, 5, 10, 50, 100];
 
-fn do_transformation<M: Transform + Default>(data: &[u8]) -> Vec<u8> {
-    let mut model: M = Default::default();
-    let result = model.transform(data).unwrap();
-    return result;
-}
-
-fn do_reverse<M: Transform + Default>(data: &[u8]) -> Vec<u8> {
-    let mut model: M = Default::default();
-    model.reverse(data).unwrap()
-}
-
-fn criterion_transform(c: &mut Criterion) {
-    let mut group = c.benchmark_group("transform");
+fn criterion_roundtrip(c: &mut Criterion) {
+    let mut group = c.benchmark_group("roundtrip");
     let source = include_bytes!("../../testdata/enwik7.raw");
     for factor in FACTORS.iter() {
         let size = factor * MIN_DATA_SIZE;
         let data: Vec<u8> = source.iter().take(size).map(|x| *x).collect();
+        let mut tmp: Vec<u8> = Vec::with_capacity(size);
         group.throughput(Throughput::Bytes(size as u64));
+
+        // Testing Run Length Transformation
+        let mut model = RunLength::new();
         group.bench_with_input(
-            BenchmarkId::new("Run-Length", size),
+            BenchmarkId::new("Run-Length (T)", size),
             data.as_slice(),
             |b, s| {
-                b.iter(|| do_transformation::<RunLength>(s));
+                b.iter(|| tmp = model.transform(s).unwrap());
             },
         );
         group.bench_with_input(
-            BenchmarkId::new("Move-to-Front", size),
+            BenchmarkId::new("Run-Length (R)", size),
+            tmp.as_slice(),
+            |b, s| {
+                b.iter(|| model.reverse(s).unwrap());
+            },
+        );
+
+        // Testing Move To Front Transformation
+        let mut model = MoveToFront::new();
+        group.bench_with_input(
+            BenchmarkId::new("Move-To-Front (T)", size),
             data.as_slice(),
             |b, s| {
-                b.iter(|| do_transformation::<MoveToFront>(s));
+                b.iter(|| tmp = model.transform(s).unwrap());
             },
         );
         group.bench_with_input(
-            BenchmarkId::new("Burrow-Wheeler", size),
+            BenchmarkId::new("Move-To-Front (R)", size),
+            tmp.as_slice(),
+            |b, s| {
+                b.iter(|| model.reverse(s).unwrap());
+            },
+        );
+
+        // Testing Burrow Wheeler Transformation
+        let mut model = BurrowWheeler::new();
+        group.bench_with_input(
+            BenchmarkId::new("Burow-Wheeler (T)", size),
             data.as_slice(),
             |b, s| {
-                b.iter(|| do_transformation::<BurrowWheeler>(s));
+                b.iter(|| tmp = model.transform(s).unwrap());
+            },
+        );
+        group.bench_with_input(
+            BenchmarkId::new("Burow-Wheeler (R)", size),
+            tmp.as_slice(),
+            |b, s| {
+                b.iter(|| model.reverse(s).unwrap());
             },
         );
     }
     group.finish();
 }
 
-fn criterion_reverse(c: &mut Criterion) {
-    let mut group = c.benchmark_group("reverse");
-    let source = include_bytes!("../../testdata/enwik7.raw");
-    for factor in FACTORS.iter() {
-        let size = factor * MIN_DATA_SIZE;
-        let data: Vec<u8> = source.iter().take(size).map(|x| *x).collect();
-        group.throughput(Throughput::Bytes(size as u64));
-        group.bench_with_input(
-            BenchmarkId::new("Run-Length", size),
-            data.as_slice(),
-            |b, s| {
-                b.iter(|| do_reverse::<RunLength>(s));
-            },
-        );
-        group.bench_with_input(
-            BenchmarkId::new("Move-To-Front", size),
-            data.as_slice(),
-            |b, s| {
-                b.iter(|| do_reverse::<MoveToFront>(s));
-            },
-        );
-    }
-    group.finish();
-}
-
-criterion_group!(transform, criterion_transform, criterion_reverse);
+criterion_group!(transform, criterion_roundtrip);
 criterion_main!(transform);
