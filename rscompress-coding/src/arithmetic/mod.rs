8ad0b3162db7c965e1fe25f4299e6fd2d100e5d0
ucyo <cayoglu@me.com>
2021-03-01 22:56:57 +0100

    Normalization should not be mandatory

--- rscompress-coding/src/arithmetic/mod.rs
+++ rscompress-coding/src/arithmetic/mod.rs
@@ -4,17 +4,16 @@ trait Model {
     fn get_current_context(&self) -> Box<dyn Context>;
 }
 
+mod fenwick;
 trait Statistics {
     fn get_freq_count(&self, symbol: u8) -> (usize, usize, usize);
     fn update_freq_count(&mut self, symbol: u8);
-    fn get_symbol(&self, target: usize) -> u8;
+    fn get_symbol(&self, target: usize) -> usize;
     fn get_total(&self) -> usize;
-    fn _normalize(&mut self);
 }
 
 trait AriCoder {
-    fn encode_symbol(&self, lower: usize, higher: usize, total: usize);
+    fn encode_symbol(&self, low: usize, high: usize, total: usize);
     fn decode_symbol(&self, total: usize) -> u8;
     fn decode_update(&mut self, symbol: u8);
-    fn _normalize(&mut self);
 }
