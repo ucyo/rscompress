3ab913a445d83995dea85352f81e27053d5bc0eb
ucyo <cayoglu@me.com>
2021-03-08 20:41:23 +0100

    Add Stistics Trait Errors

--- rscompress-coding/src/arithmetic/mod.rs
+++ rscompress-coding/src/arithmetic/mod.rs
@@ -1,3 +1,7 @@
+use std::fmt;
+use std::fmt::{Debug, Display};
+use std::error::Error;
+
 trait Context {}
 
 trait Model {
@@ -12,7 +16,36 @@ trait Statistics {
     fn get_symbol(&self, target: usize) -> &Self::Symbol;
     fn get_total(&self) -> usize;
     fn feed(&mut self, data: &[Self::Symbol]);
+
+#[derive(Debug)]
+pub enum StatisticsError {
+    /// Symbol not found in the statistics
+    UnknownSymbolError,
+    /// Error while updating the statistics
+    UpdateError,
 }
+
+impl Display for StatisticsError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            StatisticsError::UnknownSymbolError => write!(f, "Unknown Symbol"),
+            StatisticsError::UpdateError => write!(f, "Could not update Symbol"),
+        }
+    }
+}
+
+impl Error for StatisticsError {}
+
+impl From<fenwick::map::MapError> for StatisticsError {
+
+    fn from(f: fenwick::map::MapError) -> Self {
+        match f {
+            fenwick::map::MapError::UnknownIndexError => StatisticsError::UpdateError,
+            fenwick::map::MapError::UnknownSymbolError => StatisticsError::UnknownSymbolError,
+        }
+    }
+}
+
 
 trait AriCoder {
     fn encode_symbol(&self, low: usize, high: usize, total: usize);
