b84c5148da96fa302b727ba7fc6249f06d52187e
ucyo <cayoglu@me.com>
2021-03-14 16:12:07 +0100

    Add method for new calculation of intervals

--- rscompress-coding/src/arithmetic/range.rs
+++ rscompress-coding/src/arithmetic/range.rs
@@ -10,13 +10,32 @@ struct RangeEnc {
 
 impl Display for RangeEnc {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "RangeEnc(\n  low: {:032b},\n  rng: {:032b},\n)", self.low, self.rng)
+        write!(
+            f,
+            "RangeEnc(\n  low: {:032b},\n  rng: {:032b},\n)",
+            self.low, self.rng
+        )
     }
 }
 
 impl RangeEnc {
+
+    /// Create new Range Encoder
     pub fn new() -> Self {
         RangeEnc { low: 0, rng: !0 }
+    }
+
+    /// Calculate new `low` and `rng` values
+    pub fn next_interval(&self, low: u32, high: u32, total: u32) -> (INTERVAL, INTERVAL) {
+        let range = self.rng / total;
+        let new_low = self.low + low * range;
+
+        let new_rng = if low == high {
+            self.rng - new_low
+        } else {
+            (high - low) * range
+        };
+        (new_low, new_rng)
     }
 }
 
@@ -26,17 +45,21 @@ impl Default for RangeEnc {
     }
 }
 
-
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn test_printing() {
+    fn test_init() {
         let enc = RangeEnc::new();
-        println!("{}", enc);
-
         assert_eq!(enc.low, 0);
         assert_eq!(enc.rng, INTERVAL::MAX);
+    }
+    #[test]
+    fn test_update() {
+        let enc = RangeEnc::new();
+        let (l, r) = enc.next_interval(5, 10, 10);
+        assert_eq!(l, (INTERVAL::MAX >> 1) - 2);
+        assert_eq!(r, (INTERVAL::MAX >> 1));
     }
 }
